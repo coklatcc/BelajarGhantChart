bb5b98eab07a453df398a8c4cb77e17c23ea391d
coklatcc <coklatcc@gmail.com>
2018-12-09 13:29:54 +0700

    Memperbaiki code untuk menampilkan data sfsafasasda

---
+++ BelajarGhantChart/src/belajarghantchart/SAMPLEGhantController.java
@@ -0,0 +1,158 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package belajarghantchart;
+
+import java.net.URL;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.ResourceBundle;
+import javafx.embed.swing.SwingNode;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.fx.ChartViewer;
+import org.jfree.chart.fx.interaction.ChartMouseListenerFX;
+import org.jfree.data.category.IntervalCategoryDataset;
+import org.jfree.data.gantt.Task;
+import org.jfree.data.gantt.TaskSeries;
+import org.jfree.data.gantt.TaskSeriesCollection;
+import org.jfree.data.time.SimpleTimePeriod;
+
+/**
+ *
+ * @author kuupie
+ */
+public class SAMPLEGhantController implements Initializable {
+    
+    private Label label;
+    
+    @FXML
+    private Pane paneView;
+    
+        
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        loadData();
+        // TODO
+    }
+
+    private static Date date(final int day, final int month, final int year) {
+
+        final Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month, day);
+        final Date result = calendar.getTime();
+        return result;
+
+    }    
+    
+    private IntervalCategoryDataset getCategoryDataset(){
+        final TaskSeries s1 = new TaskSeries("Scheduled");
+        s1.add(new Task("Write Proposal",
+               new SimpleTimePeriod(date(1, Calendar.APRIL, 2001),
+                                    date(5, Calendar.APRIL, 2001))));
+        s1.add(new Task("Obtain Approval",
+               new SimpleTimePeriod(date(9, Calendar.APRIL, 2001),
+                                    date(9, Calendar.APRIL, 2001))));
+        s1.add(new Task("Requirements Analysis",
+               new SimpleTimePeriod(date(10, Calendar.APRIL, 2001),
+                                    date(5, Calendar.MAY, 2001))));
+        s1.add(new Task("Design Phase",
+               new SimpleTimePeriod(date(6, Calendar.MAY, 2001),
+                                    date(30, Calendar.MAY, 2001))));
+        s1.add(new Task("Design Signoff",
+               new SimpleTimePeriod(date(2, Calendar.JUNE, 2001),
+                                    date(2, Calendar.JUNE, 2001))));
+        s1.add(new Task("Alpha Implementation",
+               new SimpleTimePeriod(date(3, Calendar.JUNE, 2001),
+                                    date(31, Calendar.JULY, 2001))));
+        s1.add(new Task("Design Review",
+               new SimpleTimePeriod(date(1, Calendar.AUGUST, 2001),
+                                    date(8, Calendar.AUGUST, 2001))));
+        s1.add(new Task("Revised Design Signoff",
+               new SimpleTimePeriod(date(10, Calendar.AUGUST, 2001),
+                                    date(10, Calendar.AUGUST, 2001))));
+        s1.add(new Task("Beta Implementation",
+               new SimpleTimePeriod(date(12, Calendar.AUGUST, 2001),
+                                    date(12, Calendar.SEPTEMBER, 2001))));
+        s1.add(new Task("Testing",
+               new SimpleTimePeriod(date(13, Calendar.SEPTEMBER, 2001),
+                                    date(31, Calendar.OCTOBER, 2001))));
+        s1.add(new Task("Final Implementation",
+               new SimpleTimePeriod(date(1, Calendar.NOVEMBER, 2001),
+                                    date(15, Calendar.NOVEMBER, 2001))));
+        s1.add(new Task("Signoff",
+               new SimpleTimePeriod(date(28, Calendar.NOVEMBER, 2001),
+                                    date(30, Calendar.NOVEMBER, 2001))));
+
+        final TaskSeries s2 = new TaskSeries("Actual");
+        s2.add(new Task("Write Proposal",
+               new SimpleTimePeriod(date(1, Calendar.APRIL, 2001),
+                                    date(5, Calendar.APRIL, 2001))));
+        s2.add(new Task("Obtain Approval",
+               new SimpleTimePeriod(date(9, Calendar.APRIL, 2001),
+                                    date(9, Calendar.APRIL, 2001))));
+        s2.add(new Task("Requirements Analysis",
+               new SimpleTimePeriod(date(10, Calendar.APRIL, 2001),
+                                    date(15, Calendar.MAY, 2001))));
+        s2.add(new Task("Design Phase",
+               new SimpleTimePeriod(date(15, Calendar.MAY, 2001),
+                                    date(17, Calendar.JUNE, 2001))));
+        s2.add(new Task("Design Signoff",
+               new SimpleTimePeriod(date(30, Calendar.JUNE, 2001),
+                                    date(30, Calendar.JUNE, 2001))));
+        s2.add(new Task("Alpha Implementation",
+               new SimpleTimePeriod(date(1, Calendar.JULY, 2001),
+                                    date(12, Calendar.SEPTEMBER, 2001))));
+        s2.add(new Task("Design Review",
+               new SimpleTimePeriod(date(12, Calendar.SEPTEMBER, 2001),
+                                    date(22, Calendar.SEPTEMBER, 2001))));
+        s2.add(new Task("Revised Design Signoff",
+               new SimpleTimePeriod(date(25, Calendar.SEPTEMBER, 2001),
+                                    date(27, Calendar.SEPTEMBER, 2001))));
+        s2.add(new Task("Beta Implementation",
+               new SimpleTimePeriod(date(27, Calendar.SEPTEMBER, 2001),
+                                    date(30, Calendar.OCTOBER, 2001))));
+        s2.add(new Task("Testing",
+               new SimpleTimePeriod(date(31, Calendar.OCTOBER, 2001),
+                                    date(17, Calendar.NOVEMBER, 2001))));
+        s2.add(new Task("Final Implementation",
+               new SimpleTimePeriod(date(18, Calendar.NOVEMBER, 2001),
+                                    date(5, Calendar.DECEMBER, 2001))));
+        s2.add(new Task("Signoff",
+               new SimpleTimePeriod(date(10, Calendar.DECEMBER, 2001),
+                                    date(11, Calendar.DECEMBER, 2001))));
+                
+        final TaskSeriesCollection collection = new TaskSeriesCollection();
+        collection.add(s1);
+        collection.add(s2);
+
+        return collection;
+    }
+    
+    private void loadData(){
+        
+      paneView.getChildren().clear();
+      IntervalCategoryDataset dataset = getCategoryDataset();
+        JFreeChart chart = ChartFactory.createGanttChart("Gantt Chart Sample", "Software Development Phases", "Timeline", dataset);
+        //ChartViewer viewer = new ChartViewer(chart);
+        //paneView.add(new ChartViewer(chart));
+        SwingNode chartSwingNode = new SwingNode();
+        ChartPanel jchaChartPanel = new ChartPanel(chart);
+        jchaChartPanel.setPreferredSize(new java.awt.Dimension(800, 500));
+        chartSwingNode.setContent(jchaChartPanel);
+        
+        paneView.getChildren().addAll(chartSwingNode);
+    }
+    
+        
+}
